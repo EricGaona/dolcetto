2b0ebf86b35bff0f3d1bb94632f51813c7c088ad
ericgaona <ericgaona0413@gmail.com>
2020-06-17 14:14:51 +0000

    added home app and templates

--- dolcetto/settings.py
+++ dolcetto/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    #  apps
+    'home'
 
 ]
 
@@ -61,7 +63,10 @@ ROOT_URLCONF = 'dolcetto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
