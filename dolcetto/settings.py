89cadb6d174be4f6ee08d74b08c76d4296779f53
ericgaona <ericgaona0413@gmail.com>
2020-06-24 23:32:07 +0000

    removed secret key and set debug

--- dolcetto/settings.py
+++ dolcetto/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(t0r9e0*rahqx74=ubco8)rb1inp20(#*g4b(q1s2dh7%#*0si'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['dolcetto.herokuapp.com', 'localhost']
 
