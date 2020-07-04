f167e23c8cb179aadbc05ddc531c995ad4222585
ericgaona <ericgaona0413@gmail.com>
2020-07-04 19:37:58 +0000

    tabulate the html files and added some comments in the app home

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+"""
+This URLs was been included in the project level URLs file.
+"""
+
 urlpatterns = [
     path('', views.index, name='home')
 ]
