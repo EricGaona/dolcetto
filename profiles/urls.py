b60dd64ccdae426f04f8712d85007c118ab58d38
ericgaona <ericgaona0413@gmail.com>
2020-07-04 19:49:59 +0000

    tabulate the html files and added some comments in the app profiles

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+"""
+These URLs were been included in the project level URLs file.
+"""
+
 urlpatterns = [
     path('', views.profile, name='profile'),
     path('order_history/<order_number>', views.order_history, name='order_history'),
